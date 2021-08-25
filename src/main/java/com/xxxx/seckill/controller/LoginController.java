9698ba2be1790bac129fd52696044446fc1547ff
Study-ym <yeyufishym@gmail.com>
2021-08-25 16:36:05 +0800

    使用注解对参数校验-20210825

--- src/main/java/com/xxxx/seckill/controller/LoginController.java
+++ src/main/java/com/xxxx/seckill/controller/LoginController.java
@@ -7,9 +7,12 @@ import com.xxxx.seckill.vo.RespBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/login")
@@ -29,8 +32,7 @@ public class LoginController {
     }
     @RequestMapping("/doLogin")
     @ResponseBody
-    public RespBean doLogin(LoginVo loginVo) {
-        log.info("test");
+    public RespBean doLogin(@Valid LoginVo loginVo) {
         return userService.doLogin(loginVo);
     }
 }
