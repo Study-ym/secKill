25449a7c761f9f46306822aae14e0aa4c96bc4a0
Study-ym <yeyufishym@gmail.com>
2021-08-26 20:52:18 +0800

    分布式session实现——20210826
    Signed-off-by: Study-ym <yeyufishym@gmail.com>

--- src/main/java/com/xxxx/seckill/service/impl/UserServiceImpl.java
+++ src/main/java/com/xxxx/seckill/service/impl/UserServiceImpl.java
@@ -5,14 +5,17 @@ import com.xxxx.seckill.exception.GlobalException;
 import com.xxxx.seckill.mapper.UserMapper;
 import com.xxxx.seckill.pojo.User;
 import com.xxxx.seckill.service.IUserService;
+import com.xxxx.seckill.utils.CookieUtil;
 import com.xxxx.seckill.utils.MD5Util;
-import com.xxxx.seckill.utils.ValidatorUtil;
+import com.xxxx.seckill.utils.UUIDUtil;
 import com.xxxx.seckill.vo.LoginVo;
 import com.xxxx.seckill.vo.RespBean;
 import com.xxxx.seckill.vo.RespBeanEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * <p>
@@ -27,7 +30,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     @Autowired
     private UserMapper userMapper;
     @Override
-    public RespBean doLogin(LoginVo loginVo) {
+    public RespBean doLogin(LoginVo loginVo, HttpServletRequest request, HttpServletResponse response) {
         String mobile = loginVo.getMobile();
         String password = loginVo.getPassword();
         //参数校验
@@ -45,6 +48,10 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         if (!MD5Util.fromPassToDBPass(password, user.getSalt()).equals(user.getPassword())) {
             throw new GlobalException(RespBeanEnum.LOGIN_ERROR);
         }
+        //生成cookie
+        String ticket = UUIDUtil.uuid();
+        request.getSession().setAttribute(ticket, user);
+        CookieUtil.setCookie(request, response, "userTicket", ticket);
         return RespBean.success();
     }
 
